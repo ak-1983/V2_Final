bb8894a581c71a25b8fd019567494b8ff3d09ab2
shetkarneeraj <94823623+shetkarneeraj@users.noreply.github.com>
2024-12-16 15:00:21 +0530

    Minor changes and bug fixes
    Docker files added

--- peereval/settings.py
+++ peereval/settings.py
@@ -75,14 +75,22 @@ WSGI_APPLICATION = 'peereval.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'peereval',
+#         'USER': 'postgres',
+#         'PASSWORD': '12345678',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+# Configure this for sqlite
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'peereval',
-        'USER': 'postgres',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
